acd93a9ae2cd009f525c2aca44f66ef454ea1df1
Ubuntu <ubuntu@ip-172-31-16-81.eu-north-1.compute.internal>
2026-08-27 09:05:55 +0000

    updatedversion

--- hello_word_application/app.py
+++ hello_word_application/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World! who the fuck u are🚀"
+    return "Hello World! who the fuck u are🚀 THis is my next version :>"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
